21a5612da6c13fd84469e7b2d7c18dba491573eb
Mateuss18 <mateusdfl18@gmail.com>
2026-02-20 23:44:42 -0300

    feat(CompaniesList): render company cards dynamically from data

--- app/components/CompaniesList.vue
+++ app/components/CompaniesList.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="companies-list flex justify-between gap-8 flex-wrap">
-    <CompanyCard />
-    <CompanyCard />
-    <CompanyCard />
+  <div class="companies-list flex justify-center gap-20 flex-wrap">
+    <CompanyCard v-for="company in dataCompanies.companies" :key="company.id" :company="company" />
   </div>
 </template>
+
+<script setup lang="ts">
+import dataCompanies from '../../data/companies.json'
+</script>
