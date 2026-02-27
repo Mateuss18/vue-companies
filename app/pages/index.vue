7666efc5478f272edf8d62aa4034ef74f9537dd6
Mateuss18 <mateusdfl18@gmail.com>
2026-02-27 00:08:53 -0300

    feat: improve responsive design and add pagination to companies list

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="home min-h-screen text-tex flex flex-col justify-center px-26">
-    <HeroSection />
-    <CompaniesList />
+  <div class="home min-h-screen text-tex flex flex-col justify-center px-4 md:px-12 xxl:px-34">
+    <!-- 26 -->
+    <HeroSection @scrollToList="scrollToList" />
+    <section ref="companiesCardsListRef">
+      <CompaniesList />
+    </section>
     <FooterSimple />
   </div>
 </template>
 
-<style scoped>
-.home {
-  background: var(--color-bg) url(../assets/imgs/background-layer-v2.svg) no-repeat center/cover;
+<script setup lang="ts">
+const companiesCardsListRef = ref<HTMLElement | null>(null)
+
+const scrollToList = () => {
+  companiesCardsListRef.value?.scrollIntoView({ behavior: 'smooth' })
 }
-</style>
+</script>
