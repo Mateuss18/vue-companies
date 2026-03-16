7e3b176db7b9d50d8d2e1839d027db0f8693e183
Mateuss18 <mateusdfl18@gmail.com>
2026-03-16 01:30:37 -0300

    feat(search): add filters panel and improve search layout

---
+++ app/components/FiltersPanel.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="flex gap-10 mt-6">
+    <div class="flex flex-col gap-2 w-[50%]">
+      <p>Tags</p>
+
+      <div class="flex gap-3 flex-wrap">
+        <UButton
+          v-for="tag in tagOptions"
+          :key="tag"
+          @click="toggleTag(tag)"
+          color="primary"
+          :variant="selectedTags.includes(tag) ? 'solid' : 'outline'"
+          class="rounded-3xl py-3 px-5 outline-0 ring-0 border cursor-pointer"
+          :class="
+            selectedTags.includes(tag)
+              ? 'text-primary bg-primary/5 border-primary hover:bg-secondary/5 hover:text-secondary hover:border-secondary'
+              : 'text-white border-gray-500 hover:bg-gray-50/5'
+          "
+        >
+          {{ tag }}
+        </UButton>
+      </div>
+    </div>
+
+    <div class="flex flex-col gap-2 w-[50%]">
+      <p>Modelo de trabalho</p>
+
+      <div class="flex gap-3 flex-wrap">
+        <UButton
+          v-for="workModel in workModelOptions"
+          :key="workModel"
+          @click="toggleWorkModel(workModel)"
+          color="primary"
+          :variant="selectedWorkModels.includes(workModel) ? 'solid' : 'outline'"
+          class="rounded-3xl py-3 px-5 outline-0 ring-0 border cursor-pointer"
+          :class="
+            selectedWorkModels.includes(workModel)
+              ? 'text-primary bg-primary/5 border-primary hover:bg-secondary/5 hover:text-secondary hover:border-secondary'
+              : 'text-white border-gray-500 hover:bg-gray-50/5'
+          "
+        >
+          {{ workModel }}
+        </UButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const {
+  tagOptions,
+  toggleTag,
+  selectedTags,
+  workModelOptions,
+  toggleWorkModel,
+  selectedWorkModels,
+} = useCompanyQuery()
+</script>
---
+++ app/components/SearchFiltersBar.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="bg-bg-secondary rounded-2xl p-6 border-gray border-2 mt-6 mx-50">
+    <div class="flex gap-5 justify-between mb-4">
+      <SearchBar v-model="search" />
+
+      <div class="flex gap-3">
+        <UButton
+          size="md"
+          icon="i-lucide-sliders-horizontal"
+          class="px-5 cursor-pointer"
+          :class="openPanel ? 'bg-primary/5' : ''"
+          variant="outline"
+          @click="openPanelFilters"
+        >
+          Filtros
+          <div
+            v-if="totalFiltersSelected > 0"
+            class="circle ml-1 border-primary border rounded-2xl bg-bg-secondary text-primary text-xs"
+          >
+            {{ totalFiltersSelected }}
+          </div>
+        </UButton>
+
+        <UButton
+          class="px-5 cursor-pointer"
+          @click="clear"
+          :disabled="activeFilters.length == 0"
+          variant="outline"
+          color="error"
+        >
+          Limpar
+        </UButton>
+      </div>
+    </div>
+
+    <div class="flex justify-between">
+      <div class="flex items-center gap-2">
+        <p class="text-xs">Ativos:</p>
+
+        <div class="flex gap-2">
+          <UButton
+            v-for="selectedWorkModel in selectedWorkModels"
+            :key="selectedWorkModel"
+            class="text-xs cursor-pointer"
+            trailing-icon="i-lucide-x"
+            size="sm"
+            @click="toggleWorkModel(selectedWorkModel)"
+          >
+            {{ selectedWorkModel }}
+          </UButton>
+
+          <UButton
+            v-for="selectedTag in selectedTags"
+            :key="selectedTag"
+            class="text-xs cursor-pointer"
+            trailing-icon="i-lucide-x"
+            size="sm"
+            @click="toggleTag(selectedTag)"
+          >
+            {{ selectedTag }}
+          </UButton>
+        </div>
+      </div>
+
+      <div class="flex text-xs items-center">X resultados</div>
+    </div>
+
+    <FiltersPanel v-show="openPanel" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const { search, selectedTags, toggleTag, selectedWorkModels, toggleWorkModel, clear } =
+  useCompanyQuery()
+
+const totalFiltersSelected = computed(() => {
+  return selectedTags.value.length + selectedWorkModels.value.length
+})
+
+const activeFilters = computed(() => {
+  return [...selectedTags.value, ...selectedWorkModels.value]
+})
+
+const openPanel = ref(false)
+
+const openPanelFilters = () => {
+  openPanel.value = !openPanel.value
+}
+</script>
+
+<style scoped>
+.circle {
+  font-variant-numeric: tabular-nums;
+  padding: 3px 7px;
+}
+</style>
