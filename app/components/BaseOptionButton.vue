f55450d03328359462835f2387e65b078737e41d
Mateuss18 <mateusdfl18@gmail.com>
2026-04-04 19:35:06 -0300

    feat(components): add BaseOptionButton reusable component

---
+++ app/components/BaseOptionButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <UButton
+    color="primary"
+    :variant="selected ? 'solid' : 'outline'"
+    class="rounded-3xl py-2 px-4 outline-0 ring-0 border cursor-pointer capitalize"
+    :class="
+      selected
+        ? 'text-primary bg-primary/5 border-primary hover:bg-secondary/5 hover:text-secondary hover:border-secondary active:bg-secondary/5'
+        : 'text-white border-gray-500 hover:bg-gray-50/5 active:bg-gray-50/5'
+    "
+  >
+    {{ label }}
+  </UButton>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  label: string
+  selected?: boolean
+}
+
+defineProps<Props>()
+</script>
