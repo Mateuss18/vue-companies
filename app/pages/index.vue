67212d6f9a775635ba5ff13def0c804221cb8bd5
Mateuss18 <mateusdfl18@gmail.com>
2026-04-13 23:34:20 -0300

    feat: add filters panel and improve error page internationalization

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="home min-h-screen text-text flex flex-col justify-center px-4 md:px-12 xxl:px-34">
+  <div class="home min-h-screen text-text flex flex-col justify-center px-4 md:px-12 xxl:px-26">
     <LayoutHeroSection @scrollToList="scrollToList" />
 
     <section ref="companiesCardsListRef">
       <FiltersSearchFiltersBar />
 
-      <CompanyCompaniesList @scrollToList="scrollToList" />
+      <div class="mt-12 flex flex-col gap-8 xl:flex-row xl:items-start">
+        <FiltersPanel />
+
+        <div class="min-w-0 flex-1 min-h-full">
+          <CompanyCompaniesList @scrollToList="scrollToList" />
+        </div>
+      </div>
     </section>
 
     <LayoutFooterSimple />
